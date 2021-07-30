b6e6f0180d86dff1833862721a2885c614adbdb6
eulalio <eulalio@endcom.mx>
2021-07-30 13:11:17 -0500

    implementacion de StateProvider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { initialState, reducer } from './auth/reducer';
+import { StateProvider } from './auth/StateProvider';
 
 ReactDOM.render(
-    <App />,
+  <StateProvider initialState={ initialState } reducer={ reducer }>
+    <App />
+  </StateProvider>,
   document.getElementById('root')
 );
