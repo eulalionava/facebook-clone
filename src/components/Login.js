c2a1f8d99879d020c3ffbbd99effcd4a87d21267
eulalio <eulalio@endcom.mx>
2021-07-30 09:46:48 -0500

    authenticacion con google

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,22 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { auth, provider } from '../firebase/firebase';
 import '../styles/style.css';
 
 export const Login =()=>{
 
     const singIn = ()=>{
-
+        auth.signInWithPopup(provider)
+        .then( result=>{
+            console.log(result);
+        }).catch( (error)=>alert(error.message));
     }
     
     return(
         <div className="login">
             <div className="login__logo">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg" alt="logo"/>
-                <img src="http://1000marcas.net/wp-content/uploads/2019/12/Facebook-logo.png"/>
+                <img src="http://1000marcas.net/wp-content/uploads/2019/12/Facebook-logo.png" alt="imagen"/>
             </div>
             <Button type="submit" onClick={ singIn }>
                 SingIn
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,3 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from 'firebase';
 
 
@@ -14,3 +13,8 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth,provider };
+export default db;
